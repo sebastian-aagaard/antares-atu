f759edcfc393afd3624818d74f8aaf86841ea389
sebastian-aagaard <sebastian.aagaard@gmail.com>
2024-07-07 15:16:53 +0200

    updated default material

--- ATU/AdiraATU/configuration/machine_declaration.js
+++ ATU/AdiraATU/configuration/machine_declaration.js
@@ -18,19 +18,30 @@ exports.declareMachine = function(machineConfig)
 {
   machineConfig.setBuildstyleName('Nikon SLM Solutions');
   machineConfig.setMachineName('Antares');
-  machineConfig.addMaterialName('unspecified');
-
-
-  machineConfig.addLayerThickness(10);
-  machineConfig.addLayerThickness(20);
-  machineConfig.addLayerThickness(30);
-  machineConfig.addLayerThickness(40);
-  machineConfig.addLayerThickness(50);
-  machineConfig.addLayerThickness(60);
-  machineConfig.addLayerThickness(70);
-  machineConfig.addLayerThickness(80);
-  machineConfig.addLayerThickness(90);
-  machineConfig.addLayerThickness(100);
+  machineConfig.addMaterialName('Material 1');
+  machineConfig.addLayerThicknessToMaterial('Material 1', 10);
+  machineConfig.addLayerThicknessToMaterial('Material 1', 20);
+  machineConfig.addLayerThicknessToMaterial('Material 1', 30);
+  machineConfig.addLayerThicknessToMaterial('Material 1', 40);
+  machineConfig.addLayerThicknessToMaterial('Material 1', 50);
+  machineConfig.addLayerThicknessToMaterial('Material 1', 60);
+  machineConfig.addLayerThicknessToMaterial('Material 1', 61);
+  machineConfig.addLayerThicknessToMaterial('Material 1', 70);
+  machineConfig.addLayerThicknessToMaterial('Material 1', 80);
+  machineConfig.addLayerThicknessToMaterial('Material 1', 90);
+  machineConfig.addLayerThicknessToMaterial('Material 1', 100);
+  
+ 
+//   machineConfig.addLayerThickness(10);
+//   machineConfig.addLayerThickness(20);
+//   machineConfig.addLayerThickness(30);
+//   machineConfig.addLayerThickness(40);
+//   machineConfig.addLayerThickness(50);
+//   machineConfig.addLayerThickness(60);
+//   machineConfig.addLayerThickness(70);
+//   machineConfig.addLayerThickness(80);
+//   machineConfig.addLayerThickness(90);
+//   machineConfig.addLayerThickness(100);
 // 
 //   machineConfig.addLayerThicknessToMaterial('unspecified', 50);
 //   machineConfig.addLayerThicknessToMaterial('unspecified', 100);
@@ -39,15 +50,6 @@ exports.declareMachine = function(machineConfig)
 
 
   //machineConfig.addLayerThicknessToMaterial('IN718', 60);
-  // machineConfig.addMaterialName('unspecified');
-//   machineConfig.addLayerThicknessToMaterial('unspecified', 10);
-//   machineConfig.addLayerThicknessToMaterial('unspecified', 20);
-//   machineConfig.addLayerThicknessToMaterial('unspecified', 30);
-//   machineConfig.addLayerThicknessToMaterial('unspecified', 40);
-//   machineConfig.addLayerThicknessToMaterial('unspecified', 50);
-//   machineConfig.addLayerThicknessToMaterial('unspecified', 60);
-//   machineConfig.addLayerThicknessToMaterial('unspecified', 70);
-//   machineConfig.addLayerThicknessToMaterial('unspecified', 80);
-//   machineConfig.addLayerThicknessToMaterial('unspecified', 90);
-//   machineConfig.addLayerThicknessToMaterial('unspecified', 100);
+
+
 };
